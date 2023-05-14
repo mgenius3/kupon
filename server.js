3b6f85c850acb8e21a0385f1dba77b9d5370f705
menius3 <benmos16@gmail.com>
2023-05-14 21:33:59 +0100

    server

--- server.js
+++ server.js
@@ -1,84 +1,13 @@
-// const express = require('express');
-// const path = require('path');
-// // const bodyParser = require('body-parser');
-// // const next = require('next');
-// // const dotenv = require('dotenv');
-// // const dev = process.env.NODE_ENV !== 'production';
-
-// // dotenv.config();
-// // const app = next({ dev });
-// // const handle = app.getRequestHandler();
-// const port = process.env.PORT || 3000;
-// const errorHandler = require('./middleware/errorHandler');
-// const morgan = require('morgan');
-// const { createTableUser } = require('./database/auth');
-// const { createTableLogistics } = require('./database/logistics');
-// const { createTableSell } = require('./database/sell');
-
-// // app.prepare().then(() => {
-// const server = express();
-
-// //Create Table for User
-// createTableUser();
-// // createFileUser();
-// createTableLogistics();
-// createTableSell();
-
-// server.use(morgan('dev'));
-// // Middleware to parse JSON request body
-// // server.use(bodyParser.json());
-// // Default route handler
-// server.use(express.static(path.join(__dirname, 'out')));
-// server.use('/styles', express.static(path.join(__dirname, '/styles')));
-// server.use(express.json({ limit: '10mb' }));
-// server.use('/logistic', require('./routes/logistics'));
-// //Create Table for Sell
-// server.use('/sell', require('./routes/sell'));
-// server.use('/user', require('./routes/auth'));
-// server.use('/admin', require('./routes/admin'));
-
-// //Create Table for Logistics
-
-// // error handling
-// server.use(errorHandler);
-
-// // Middleware to handle requests that don't match any route
-// server.use(function (req, res) {
-//   res.status(404);
-
-//   // Render a custom error page
-//   res.json({
-//     title: 'Page Not Found',
-//     message: 'The requested page does not exist',
-//   });
-// });
-
-// // Error handler middleware
-// server.use(function (err, req, res) {
-//   console.error(err.stack);
-//   res.status(500);
-
-//   // render a custom error page
-//   res.json({
-//     title: 'Server Error',
-//     message: 'Something went wrong on our end',
-//   });
-// });
-
-// server.listen(port, (err) => {
-//   if (err) throw err;
-//   console.log(`> Ready on http://localhost:${port}`);
-// });
-
 const express = require('express');
+const path = require('path');
 // const bodyParser = require('body-parser');
-const next = require('next');
-const dotenv = require('dotenv');
-const dev = process.env.NODE_ENV !== 'production';
+// const next = require('next');
+// const dotenv = require('dotenv');
+// const dev = process.env.NODE_ENV !== 'production';
 
-dotenv.config();
-const app = next({ dev });
-const handle = app.getRequestHandler();
+// dotenv.config();
+// const app = next({ dev });
+// const handle = app.getRequestHandler();
 const port = process.env.PORT || 3000;
 const errorHandler = require('./middleware/errorHandler');
 const morgan = require('morgan');
@@ -86,60 +15,131 @@ const { createTableUser } = require('./database/auth');
 const { createTableLogistics } = require('./database/logistics');
 const { createTableSell } = require('./database/sell');
 
-app.prepare().then(() => {
-  const server = express();
+// app.prepare().then(() => {
+const server = express();
 
-  //Create Table for User
-  createTableUser();
-  // createFileUser();
-  createTableLogistics();
-  createTableSell();
+//Create Table for User
+createTableUser();
+// createFileUser();
+createTableLogistics();
+createTableSell();
 
-  server.use(morgan('dev'));
-  // Middleware to parse JSON request body
-  // server.use(bodyParser.json());
-  server.use(express.json({ limit: '10mb' }));
-  server.use('/logistic', require('./routes/logistics'));
-  //Create Table for Sell
-  server.use('/sell', require('./routes/sell'));
-  server.use('/user', require('./routes/auth'));
-  server.use('/admin', require('./routes/admin'));
+server.use(morgan('dev'));
+// Middleware to parse JSON request body
+// server.use(bodyParser.json());
+// Default route handler
+server.use(express.static(path.join(__dirname, 'out')));
+server.use('/styles', express.static(path.join(__dirname, '/styles')));
+server.use(express.json({ limit: '10mb' }));
+server.use('/logistic', require('./routes/logistics'));
+//Create Table for Sell
+server.use('/sell', require('./routes/sell'));
+server.use('/user', require('./routes/auth'));
+server.use('/admin', require('./routes/admin'));
 
-  //Create Table for Logistics
+//Create Table for Logistics
 
-  // error handling
-  server.use(errorHandler);
+// error handling
+server.use(errorHandler);
 
-  // Default route handler
-  server.get('*', (req, res) => {
-    return handle(req, res);
-  });
+// Middleware to handle requests that don't match any route
+server.use(function (req, res) {
+  res.status(404);
 
-  // Middleware to handle requests that don't match any route
-  server.use(function (req, res) {
-    res.status(404);
-
-    // Render a custom error page
-    res.json({
-      title: 'Page Not Found',
-      message: 'The requested page does not exist',
-    });
-  });
-
-  // Error handler middleware
-  server.use(function (err, req, res) {
-    console.error(err.stack);
-    res.status(500);
-
-    // render a custom error page
-    res.json({
-      title: 'Server Error',
-      message: 'Something went wrong on our end',
-    });
-  });
-
-  server.listen(port, (err) => {
-    if (err) throw err;
-    console.log(`> Ready on http://localhost:${port}`);
+  // Render a custom error page
+  res.json({
+    title: 'Page Not Found',
+    message: 'The requested page does not exist',
   });
 });
+
+// Error handler middleware
+server.use(function (err, req, res) {
+  console.error(err.stack);
+  res.status(500);
+
+  // render a custom error page
+  res.json({
+    title: 'Server Error',
+    message: 'Something went wrong on our end',
+  });
+});
+
+server.listen(port, (err) => {
+  if (err) throw err;
+  console.log(`> Ready on http://localhost:${port}`);
+});
+
+// const express = require('express');
+// // const bodyParser = require('body-parser');
+// const next = require('next');
+// const dotenv = require('dotenv');
+// const dev = process.env.NODE_ENV !== 'production';
+
+// dotenv.config();
+// const app = next({ dev });
+// const handle = app.getRequestHandler();
+// const port = process.env.PORT || 3000;
+// const errorHandler = require('./middleware/errorHandler');
+// const morgan = require('morgan');
+// const { createTableUser } = require('./database/auth');
+// const { createTableLogistics } = require('./database/logistics');
+// const { createTableSell } = require('./database/sell');
+
+// app.prepare().then(() => {
+//   const server = express();
+
+//   //Create Table for User
+//   createTableUser();
+//   // createFileUser();
+//   createTableLogistics();
+//   createTableSell();
+
+//   server.use(morgan('dev'));
+//   // Middleware to parse JSON request body
+//   // server.use(bodyParser.json());
+//   server.use(express.json({ limit: '10mb' }));
+//   server.use('/logistic', require('./routes/logistics'));
+//   //Create Table for Sell
+//   server.use('/sell', require('./routes/sell'));
+//   server.use('/user', require('./routes/auth'));
+//   server.use('/admin', require('./routes/admin'));
+
+//   //Create Table for Logistics
+
+//   // error handling
+//   server.use(errorHandler);
+
+//   // Default route handler
+//   server.get('*', (req, res) => {
+//     return handle(req, res);
+//   });
+
+//   // Middleware to handle requests that don't match any route
+//   server.use(function (req, res) {
+//     res.status(404);
+
+//     // Render a custom error page
+//     res.json({
+//       title: 'Page Not Found',
+//       message: 'The requested page does not exist',
+//     });
+//   });
+
+//   // Error handler middleware
+//   server.use(function (err, req, res) {
+//     console.error(err.stack);
+//     res.status(500);
+
+//     // render a custom error page
+//     res.json({
+//       title: 'Server Error',
+//       message: 'Something went wrong on our end',
+//     });
+//   });
+
+//   server.listen(port, (err) => {
+//     if (err) throw err;
+//     console.log(`> Ready on http://localhost:${port}`);
+//   });
+// });
