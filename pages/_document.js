086cb0e14e99b2e60cd33e51044f9fddc4a1cc40
mgenius3 <benmos16@gmail.com>
2023-07-23 10:55:02 +0000

    seo google verification

--- pages/_document.js
+++ pages/_document.js
@@ -17,6 +17,10 @@ class MyDocument extends Document {
           <link rel="stylesheet" href="/styles/plugins.css" />
           <link rel="stylesheet" href="/styles/responsive.css" />
           <link rel="stylesheet" href="/styles/shop.css" />
+          <meta
+            name="google-site-verification"
+            content="tSVeUWP1-wY-Bn3QfbEZQXs-SbwW2tTHnutRruBJ-So"
+          />
         </Head>
         <div
           id="modalOverly"
